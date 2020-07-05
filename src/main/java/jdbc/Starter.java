868f479e5929f5c6ad79fa2cf6b37ca91de9dd73
igoryavk <igoryavk@gmail.com>
2020-07-05 12:34:48 +0300

    Добавлен файл конфигурации ApplicationContext и на его основе создается объект Спринг Контекста

---
+++ src/main/java/jdbc/Starter.java
@@ -0,0 +1,10 @@
+package jdbc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Starter {
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("context.xml");
+    }
+}
